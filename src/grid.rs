2904933045035e2e54227e8b7c8b5571a81daa05
Brendan Westley <brendan@westley.us>
2022-09-27 16:50:46 -0500

    Add optimal move determination

--- src/grid.rs
+++ src/grid.rs
@@ -233,6 +233,217 @@ impl Grid {
     }
 
     pub fn get_optimal_moves(&self) -> HashSet<Wall> {
-        HashSet::new()
+        let mut optimal_moves = HashSet::new();
+
+        // Find unset walls on squares with three set walls.
+        println!("Searching for creatable boxes.");
+        for column in 0..(self.column_count - 1) {
+            for row in 0..(self.row_count - 1) {
+                if self.get_wall_count(column, row) == 3 {
+                    // The square has three walls. Fill in the last wall to complete the square.
+                    print!("    Coordinate: ({column}, {row}), Walls:");
+                    let square_walls = self.get_square_walls(column, row);
+                    // Exactly one of the below conditions should evaluate to true.
+                    if !square_walls.top.set {
+                        print!(" top");
+                        optimal_moves.insert(square_walls.top);
+                    }
+                    if !square_walls.right.set {
+                        print!(" right");
+                        optimal_moves.insert(square_walls.right);
+                    }
+                    if !square_walls.bottom.set {
+                        print!(" bottom");
+                        optimal_moves.insert(square_walls.bottom);
+                    }
+                    if !square_walls.left.set {
+                        print!(" left");
+                        optimal_moves.insert(square_walls.left);
+                    }
+                    println!(".");
+                }
+            }
+        }
+        if optimal_moves.len() > 0 {
+            return optimal_moves;
+        }
+
+        // Find walls with adjacent squares with less than two walls.
+        println!("Searching for safe moves.");
+        for column in 0..(self.column_count - 1) {
+            for row in 0..(self.row_count - 1) {
+                if self.get_wall_count(column, row) < 2 {
+                    // The square has less than two walls.
+                    print!("    Coordinate: ({column}, {row}), Walls:");
+                    let square_walls = self.get_square_walls(column, row);
+                    if !square_walls.top.set
+                        && (row == 0 || self.get_wall_count(column, row - 1) < 2)
+                    {
+                        // The top wall is not set and the above square (if one exists) has less than two walls.
+                        print!(" top");
+                        optimal_moves.insert(square_walls.top);
+                    }
+                    if !square_walls.right.set
+                        && (column == self.column_count - 2
+                        || self.get_wall_count(column + 1, row) < 2)
+                    {
+                        // The right wall is not set and the right square (if one exists) has less than two walls.
+                        print!(" right");
+                        optimal_moves.insert(square_walls.right);
+                    }
+                    if !square_walls.bottom.set
+                        && (row == self.row_count - 2 || self.get_wall_count(column, row + 1) < 2)
+                    {
+                        // The bottom wall is not set and the below square (if one exists) has less than two walls.
+                        print!(" bottom");
+                        optimal_moves.insert(square_walls.bottom);
+                    }
+                    if !square_walls.left.set
+                        && (column == 0 || self.get_wall_count(column - 1, row) < 2)
+                    {
+                        // The left wall is not set and the left square (if one exists) has less than two walls.
+                        print!(" left");
+                        optimal_moves.insert(square_walls.left);
+                    }
+                    println!(".");
+                }
+            }
+        }
+        if optimal_moves.len() > 0 {
+            return optimal_moves;
+        }
+
+        // Find walls that each trigger a shortest chain (multiple chains may have the least length).
+        // Save grid data because it will be temporarily mutated when determining chains (this helps
+        // simplify cases where chains wrap around). When a chain ends along its length (not at its
+        // other end to form a perfect loop) walls in its "tail" will result in a shorter chain.
+
+        fn get_chain_length(temporary_grid: &mut Grid, mut column: i32, mut row: i32) -> i32 {
+            println!("    Evaluating chain ({column}, {row}):");
+            let mut chain_length = 0;
+
+            // While the current position is in squares and the current square has three walls.
+            while column >= 0
+                && column < temporary_grid.column_count - 1
+                && row >= 0
+                && row < temporary_grid.row_count - 1
+                && temporary_grid.get_wall_count(column, row) == 3
+            {
+                print!("        Coordinate: ({column}, {row}), Length: {chain_length}. Setting ");
+
+                // Set the unset wall and move in that direction to the next square.
+                let square_walls = temporary_grid.get_square_walls(column, row);
+                if !square_walls.top.set {
+                    println!("top");
+                    temporary_grid.set_wall_with_wall(&square_walls.top, true);
+                    row -= 1;
+                } else if !square_walls.right.set {
+                    println!("right");
+                    temporary_grid.set_wall_with_wall(&square_walls.right, true);
+                    column += 1;
+                } else if !square_walls.bottom.set {
+                    println!("bottom");
+                    temporary_grid.set_wall_with_wall(&square_walls.bottom, true);
+                    row += 1;
+                } else if !square_walls.left.set {
+                    println!("left");
+                    temporary_grid.set_wall_with_wall(&square_walls.left, true);
+                    column -= 1;
+                } else {
+                    println!("none");
+                    break;
+                }
+
+                // A square must have been made. Increase the length of this chain.
+                chain_length += 1;
+
+                // Handles the edge case where the last line results in two boxes instead of one.
+                if column >= 0
+                    && column < temporary_grid.column_count - 1
+                    && row >= 0
+                    && row < temporary_grid.row_count - 1
+                    && temporary_grid.get_wall_count(column, row) == 4
+                {
+                    chain_length += 1;
+                }
+            }
+
+            println!("        Chain length: {chain_length}.");
+            chain_length
+        }
+
+        // Get the length of each chain triggered by a wall.
+        println!("Searching for the shortest chains.");
+        let mut chain_lengths: HashMap<Wall, i32> = HashMap::new();
+        for column in 0..(self.column_count - 1) {
+            for row in 0..(self.row_count - 1) {
+                if self.get_wall_count(column, row) == 2 {
+                    // The square has two walls.
+
+                    let first_square = self.get_square_walls(column, row);
+
+                    // For each unset of wall of this square, calculate (if not already done) the length of the chain triggered if it was set.
+                    // Both the length of "sides" of the chain need to be calculated. The first starts in the square we are already in and
+                    // the second starts in the square in direction of the triggering wall.
+                    if !first_square.top.set && !chain_lengths.contains_key(&first_square.top) {
+                        let chain_start = first_square.top;
+                        let mut temporary_grid = self.clone();
+                        temporary_grid.set_wall_with_wall(&chain_start, true);
+                        chain_lengths.insert(
+                            chain_start,
+                            get_chain_length(&mut temporary_grid, column, row)
+                                + get_chain_length(&mut temporary_grid, column, row - 1),
+                        );
+                    }
+                    if !first_square.right.set && !chain_lengths.contains_key(&first_square.right) {
+                        let chain_start = first_square.right;
+                        let mut temporary_grid = self.clone();
+                        temporary_grid.set_wall_with_wall(&chain_start, true);
+                        chain_lengths.insert(
+                            chain_start,
+                            get_chain_length(&mut temporary_grid, column, row)
+                                + get_chain_length(&mut temporary_grid, column + 1, row),
+                        );
+                    }
+                    if !first_square.bottom.set && !chain_lengths.contains_key(&first_square.bottom)
+                    {
+                        let chain_start = first_square.bottom;
+                        let mut temporary_grid = self.clone();
+                        temporary_grid.set_wall_with_wall(&chain_start, true);
+                        chain_lengths.insert(
+                            chain_start,
+                            get_chain_length(&mut temporary_grid, column, row)
+                                + get_chain_length(&mut temporary_grid, column, row + 1),
+                        );
+                    }
+                    if !first_square.left.set && !chain_lengths.contains_key(&first_square.left) {
+                        let chain_start = first_square.left;
+                        let mut temporary_grid = self.clone();
+                        temporary_grid.set_wall_with_wall(&chain_start, true);
+                        chain_lengths.insert(
+                            chain_start,
+                            get_chain_length(&mut temporary_grid, column, row)
+                                + get_chain_length(&mut temporary_grid, column - 1, row),
+                        );
+                    }
+                }
+            }
+        }
+
+        // Get the walls that each trigger the shortest chain.
+        let minimum_length = *chain_lengths.values().min().unwrap_or(&0);
+        for (wall, length) in chain_lengths {
+            println!(
+                "    {} ({}, {}) generates a chain with length {length}.",
+                if wall.is_column { "Column" } else { "Row" },
+                wall.column,
+                wall.row
+            );
+            if length == minimum_length {
+                optimal_moves.insert(wall);
+            }
+        }
+
+        optimal_moves
     }
 }
